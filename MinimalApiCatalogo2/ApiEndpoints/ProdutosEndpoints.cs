40d53363e402911da2da7d97ad65808b2e57c56b
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-14 10:28:07 -0300

    Continuação sepraração das responsabilida… …des e refatorando a class Program

---
+++ MinimalApiCatalogo2/ApiEndpoints/ProdutosEndpoints.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiCatalogo2.Context;
+using MinimalApiCatalogo2.Models;
+
+namespace MinimalApiCatalogo2.ApiEndpoints;
+
+public static class ProdutosEndpoints
+{
+    public static void MapProdutosEndpoints(this WebApplication app)
+    {
+        //---------------ENDPOINTS PARA PRODUTOS---------------------
+        app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
+
+        app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
+        {
+            return await db.Produtos.FindAsync(id)
+                 is Produto produto
+                ? Results.Ok(produto)
+                : Results.NotFound();
+        }).WithTags("Produtos");
+
+        app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
+        {
+            db.Produtos.Add(produto);
+            await db.SaveChangesAsync();
+            return Results.Created($"/categorias/{produto.ProdutoId}", produto);
+        }).WithTags("Produtos");
+
+        app.MapPut("/produtoss/{id:int}", async (int id, Produto produto, AppDbContext db) =>
+        {
+            if (produto.ProdutoId != id)
+            {
+                return Results.BadRequest();
+            }
+            var produtosDB = await db.Produtos.FindAsync(id);
+            if (produtosDB is null) return Results.NotFound();
+
+            produtosDB.Nome = produto.Nome;
+            produtosDB.Descricao = produto.Descricao;
+
+            await db.SaveChangesAsync();
+            return Results.Ok(produtosDB);
+        }).WithTags("Produtos");
+
+        app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
+        {
+            if (produto.ProdutoId != id)
+            {
+                return Results.BadRequest();
+            }
+            var produtoDB = await db.Produtos.FindAsync(id);
+            if (produtoDB is null) return Results.NotFound();
+
+            produtoDB.Nome = produto.Nome;
+            produtoDB.Descricao = produto.Descricao;
+
+            await db.SaveChangesAsync();
+            return Results.Ok(produtoDB);
+        }).WithTags("Produtos");
+
+        app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
+        {
+            var produto = await db.Produtos.FindAsync(id);
+            if (produto is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.Produtos.Remove(produto);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        }).WithTags("Produtos");
+
+    }
+}
--- MinimalApiCatalogo2/Program.cs
+++ MinimalApiCatalogo2/Program.cs
@@ -1,168 +1,26 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+
 using MinimalApiCatalogo2.ApiEndpoints;
-using MinimalApiCatalogo2.Context;
-using MinimalApiCatalogo2.Models;
-using MinimalApiCatalogo2.Services;
-using System.Text;
+using MinimalApiCatalogo2.AppServicesExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Configuring Swagger/OpenAPI  
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(connectionString: "Data Source=PRILLNOTEBOOK28\\SQLEXPRESS;Initial Catalog=MinimalApiCatalogoDB;Integrated Security=True;TrustServerCertificate=true"));
-/*Desativar a Verificação SSL (Não Recomendado): Esta é uma opção que você pode considerar apenas
-em ambientes de desenvolvimento e nunca em produção. Você pode desativar a verificação SSL 
-adicionando "TrustServerCertificate=true" à sua string de conexão. No entanto, isso torna a conexão menos segura.*/
-
-//Registrando Serviço Jwt
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-//Registrando serviços de autenticações e validação de Token
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey
-        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
-    };
-});
-builder.Services.AddAuthorization();
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build();
+
 app.MapAutenticacaoEndpoints();
+app.MapCategoriasEndpoints();
+app.MapProdutosEndpoints();
 
-app.MapGet("/", () => "Catálogo de Produtos - 2023").ExcludeFromDescription();
+var environment = app.Environment;
+app.UseExceptionHandling(environment)
+    .UseSwaggerMiddleware()
+    .UseAppCors();
 
-app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync()).WithTags("Categorias").RequireAuthorization();
 
-app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Categorias.FindAsync(id)
-       is Categoria categoria
-       ? Results.Ok(categoria)
-       : Results.NotFound();
-}).WithTags("Categorias");
-
-app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
-{
-    db.Categorias.Add(categoria);
-    await db.SaveChangesAsync();
-    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
-}).WithTags("Categorias");
-
-app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
-{
-    if (categoria.CategoriaId != id)
-    {
-        return Results.BadRequest();
-    }
-    var categoriaDB = await db.Categorias.FindAsync(id);
-    if (categoriaDB is null) return Results.NotFound();
-
-    categoriaDB.Nome = categoria.Nome;
-    categoriaDB.Descricao = categoria.Descricao;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(categoriaDB);
-}).WithTags("Categorias");
-
-app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
-{
-    var categoria = await db.Categorias.FindAsync(id);
-    if (categoria is null)
-    {
-        return Results.NotFound();
-    }
-
-    db.Categorias.Remove(categoria);
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-}).WithTags("Categorias");
-
-//---------------ENDPOINTS PARA PRODUTOS---------------------
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
-
-app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    return await db.Produtos.FindAsync(id)
-         is Produto produto
-        ? Results.Ok(produto)
-        : Results.NotFound();
-}).WithTags("Produtos");
-
-app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
-{
-    db.Produtos.Add(produto);
-    await db.SaveChangesAsync();
-    return Results.Created($"/categorias/{produto.ProdutoId}", produto);
-}).WithTags("Produtos");
-
-app.MapPut("/produtoss/{id:int}", async (int id, Produto produto, AppDbContext db) =>
-{
-    if (produto.ProdutoId != id)
-    {
-        return Results.BadRequest();
-    }
-    var produtosDB = await db.Produtos.FindAsync(id);
-    if (produtosDB is null) return Results.NotFound();
-
-    produtosDB.Nome = produto.Nome;
-    produtosDB.Descricao = produto.Descricao;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(produtosDB);
-}).WithTags("Produtos");
-
-app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
-{
-    if (produto.ProdutoId != id)
-    {
-        return Results.BadRequest();
-    }
-    var produtoDB = await db.Produtos.FindAsync(id);
-    if (produtoDB is null) return Results.NotFound();
-
-    produtoDB.Nome = produto.Nome;
-    produtoDB.Descricao = produto.Descricao;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(produtoDB);
-}).WithTags("Produtos");
-
-app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
-{
-    var produto = await db.Produtos.FindAsync(id);
-    if (produto is null)
-    {
-        return Results.NotFound();
-    }
-
-    db.Produtos.Remove(produto);
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-}).WithTags("Produtos");
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 //Ativando o serviço de autenticação e autorização
 app.UseAuthentication();
 app.UseAuthorization();
